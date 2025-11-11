655e8772283aabb767ff88fdde8f1e21aae7ea86
theyheartyonnie <kevionna.matthews@livingstoncollegiate.org>
2025-11-11 18:09:07 +0000

    refactor to create 100 circles using loops

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -41,7 +41,9 @@ var init = function (window) {
 
 
         // TODO 7 : Use a loop to create multiple circles
-
+      for (var i = 0; i < 100; i++){
+        drawCircle();
+      }
 
 
 
@@ -56,22 +58,15 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the position of each circle using physikz.updatePosition()
-           physikz.updatePosition(circles[0]);
-           physikz.updatePosition(circles[1]);
-           physikz.updatePosition(circles[2]);
-           physikz.updatePosition(circles[3]);
-           physikz.updatePosition(circles[4]);
-            
+           
             // TODO 5 : Call game.checkCirclePosition() on your circles
-           game.checkCirclePosition(circles[0]);
-           game.checkCirclePosition(circles[1]);
-           game.checkCirclePosition(circles[2]);
-           game.checkCirclePosition(circles[3]);
-           game.checkCirclePosition(circles[4]);
-    
+          
 
             // TODO 8 / TODO 9 : Iterate over the array
-           
+           for(var i = 0; i < circles.length; i++){
+            physikz.updatePosition(circles [i])
+            game.checkCirclePosition(circles [i])
+           }
             
         }
     
